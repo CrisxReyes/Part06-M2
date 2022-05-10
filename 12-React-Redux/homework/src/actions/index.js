26ede94b26e46060e0a8dc276be5eb9aefa6df80
Cristopher Reyes <critopher.smnyl@gmail.com>
2022-05-10 18:22:27 -0500

    Actions en proceso

---
+++ 12-React-Redux/homework/src/actions/index.js
@@ -0,0 +1,18 @@
+const ADD_MOVIE_FAVORITE = 'ADD_MOVIE_FAVORITE';
+const GET_MOVIES = 'GET_MOVIES';
+const GET_MOVIE_DETAIL = 'GET_MOVIE_DETAIL';
+const REMOVE_MOVIE_FAVORITE = 'REMOVE_MOVIE_FAVORITE';
+
+export function addMovieFavorite(payload) {
+    return { type: ADD_MOVIE_FAVORITE, payload };
+  }
+  
+  export function getMovies(titulo) {
+    return function(dispatch) {
+      return fetch("http://www.omdbapi.com/?apikey=b1d4d970&s=" + titulo)
+        .then(response => response.json())
+        .then(json => {
+          dispatch({ type: GET_MOVIES, payload: json });
+        });
+    };
+  }
